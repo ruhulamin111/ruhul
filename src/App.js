ee319fca16234fdf5fdb2df5646936fdc865b55f
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-15 17:37:30 +0600

    style add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import About from './components/About/About';
+import Blog from './components/Blog/Blog';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home';
+import Portfolio from './components/Portfolio/Portfolio';
+import Pricing from './components/Pricing/Pricing';
+import Resume from './components/Resume/Resume';
+import Services from './components/Services/Services';
+import Sidebar from './components/Sidebar/Sidebar';
+import Testimonial from './components/Testimonial/Testimonial';
 
 function App() {
   return (
-    <div>
-      <h2>ruhul juwel</h2>
-    </div>
+    <>
+      <Sidebar />
+      <main className='main'>
+        <Home />
+        <About />
+        <Services />
+        <Resume />
+        <Portfolio />
+        <Pricing />
+        <Testimonial />
+        <Blog />
+        <Contact />
+
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Pricing/Pricing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Pricing.css'
+
+const Pricing = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Pricing
