30e7f722fb97b5e3479df76c98ccb84a71557550
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-16 07:46:04 +0600

    resume add

---
+++ src/components/Resume/Data.js
@@ -0,0 +1,54 @@
+import { FaBellSlash, FaBuromobelexperte, FaConnectdevelop, FaCouch, FaDiscourse, FaGraduationCap, FaUniversity } from 'react-icons/fa';
+
+const Data = [
+    {
+        id: 1,
+        category: "education",
+        icon: <FaGraduationCap />,
+        year: "2019-present",
+        title: "academic degree",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur consequatur dolore harum optio iusto fuga."
+    },
+    {
+        id: 2,
+        category: "education",
+        icon: <FaUniversity />,
+        year: "2013-2017",
+        title: "bachelor degree",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur consequatur dolore harum optio iusto fuga."
+    },
+    {
+        id: 3,
+        category: "education",
+        icon: <FaConnectdevelop />,
+        year: "2019-present",
+        title: "academic degree",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur consequatur dolore harum optio iusto fuga."
+    },
+    {
+        id: 4,
+        category: "experience",
+        icon: <FaBellSlash />,
+        year: "2019-present",
+        title: "academic degree",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur consequatur dolore harum optio iusto fuga."
+    },
+    {
+        id: 5,
+        category: "experience",
+        icon: <FaDiscourse />,
+        year: "2019-present",
+        title: "frontend degree",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur consequatur dolore harum optio iusto fuga."
+    },
+    {
+        id: 6,
+        category: "experience",
+        icon: <FaBuromobelexperte />,
+        year: "2019-present",
+        title: "backend degree",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur consequatur dolore harum optio iusto fuga."
+    }
+]
+
+export default Data;
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import Card from './Card'
 import './Resume.css'
+import Data from './Data'
+
 
 const Resume = () => {
     return (
-        <div>
-            Resume
-        </div>
+        <section className='resume container section' id='resume'>
+            <h2 className="section-title">Experience</h2>
+
+            <div className="resume_container grid">
+                <div className="timeline grid">
+                    {Data.map((value, id) => {
+                        if (value.category === "education") {
+                            return (
+                                <Card key={id} title={value.title} icon={value.icon} year={value.year} desc={value.desc} />
+                            )
+                        }
+                    })}
+                </div>
+
+                <div className="timeline grid">
+                    {Data.map((value, index) => {
+                        if (value.category === "experience") {
+                            return (
+                                <Card key={index} title={value.title} icon={value.icon} year={value.year} desc={value.desc} />
+                            )
+                        }
+                    })}
+                </div>
+            </div>
+
+        </section>
     )
 }
 
