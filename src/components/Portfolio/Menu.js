487d818b09429dbc2297301f982679b24353c755
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-16 09:04:27 +0600

    work part complete

---
+++ src/components/Portfolio/Menu.js
@@ -0,0 +1,49 @@
+
+import work1 from '../../assets/work-1.svg'
+import work2 from '../../assets/work-2.svg'
+import work3 from '../../assets/work-3.svg'
+import work4 from '../../assets/work-4.svg'
+import work5 from '../../assets/work-5.svg'
+import work6 from '../../assets/work-6.svg'
+
+
+const Menu = [
+    {
+        id: 1,
+        image: work1,
+        title: "Project Management Illustration",
+        category: 'Everything'
+    },
+    {
+        id: 2,
+        image: work2,
+        title: "Project Management Illustration",
+        category: 'Web Desing'
+    },
+    {
+        id: 3,
+        image: work3,
+        title: "Project Management Illustration",
+        category: 'Branding'
+    },
+    {
+        id: 4,
+        image: work4,
+        title: "Project Management Illustration",
+        category: 'Art'
+    },
+    {
+        id: 5,
+        image: work5,
+        title: "Project Management Illustration",
+        category: 'Logo'
+    },
+    {
+        id: 6,
+        image: work6,
+        title: "Project Management Illustration",
+        category: 'Creative'
+    }
+]
+
+export default Menu
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaLink } from 'react-icons/fa'
+import Menu from './Menu'
+import './Portfolio.css'
 
 const Portfolio = () => {
+    const [items, setItems] = useState(Menu)
+
     return (
-        <div>
-            Portfolio
-        </div>
+        <section className='work container section' id='work'>
+            <h2 className='section-title'>Recent Works</h2>
+            <div className="work_filters">
+                <span className="work_item">Everything</span>
+                <span className="work_item">Creative</span>
+                <span className="work_item">Art</span>
+                <span className="work_item">Web Desing</span>
+                <span className="work_item">Branding</span>
+                <span className="work_item">Logo</span>
+            </div>
+            <div className="work_container grid">
+                {items.map((element, i) => {
+                    const { id, image, title, category } = element;
+                    return (
+                        <div className="work_card" key={i}>
+                            <div className="work_thumbnail">
+                                <img src={image} alt="" className="work_img" />
+                                <div className="work_mask"></div>
+                            </div>
+                            <span className="work_category">{category}</span>
+                            <h3 className="work_title">{title}</h3>
+                            <a href="" className="work_button">
+                                <i className="work_button_icon"><FaLink /></i>
+                            </a>
+
+                        </div>
+                    )
+                })}
+            </div>
+
+        </section>
     )
 }
 
